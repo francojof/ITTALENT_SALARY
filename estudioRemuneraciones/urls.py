f667715bf43803fa430da651aae44081f8b8cd97
francojof <francojof2@gmail.com>
2020-11-17 16:45:51 -0300

    mas cambios

--- estudioRemuneraciones/urls.py
+++ estudioRemuneraciones/urls.py
@@ -5,7 +5,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('formulario/',views.index, name="formulario"),
+    path('',views.index, name="IT_Talent_Salary"),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
