63a9be0f439fad3259931bc724be76068d430dcf
francojof <francojof2@gmail.com>
2020-11-22 18:58:33 -0300

    try catch

--- formularioPersona/views.py
+++ formularioPersona/views.py
@@ -30,62 +30,67 @@ import re
 def index(request):
     form = FormularioP()
     if request.method == 'POST':
-        
-        form=FormularioP(request.POST)
-        date_joined = datetime.now()
-        formatted_datetime = formats.date_format(date_joined, "SHORT_DATETIME_FORMAT")
-        nombre= request.POST.get('nombre')
-        email = request.POST.get('email')
-        tamano_empresa = request.POST.get('tamano_empresa')
-        pais = request.POST.get('pais')
-        edad = request.POST.get('edad')
-        estudios = request.POST.get('estudios')
-        genero = request.POST.get('genero')
-        ingles_hablado = request.POST.get('ingles_hablado')
-        ingles_escrito = request.POST.get('ingles_escrito')
-        actividad = request.POST.get('actividad') 
-        contrato = request.POST.get('contrato') 
-        cargo = request.POST.get('cargo')
-        experiencia = request.POST.get('experiencia')
-        rentaLiquida = request.POST.get('rentaLiquida')
-        rentaBono = request.POST.get('rentaBono')
-        duracionTrabajo = request.POST.get('duracionTrabajo')
-        BeneficiosEmpleador = request.POST.get('BeneficiosEmpleador')
-        FactoresCambioEmpleo = request.POST.get('FactoresCambioEmpleo')
-        template= get_template('correo.html')
-        
-        if(nombre==""):
-            messages.error(request, 'Debe ingresar un nombre')
+        try:
+            form=FormularioP(request.POST)
+            date_joined = datetime.now()
+            formatted_datetime = formats.date_format(date_joined, "SHORT_DATETIME_FORMAT")
+            nombre= request.POST.get('nombre')
+            email = request.POST.get('email')
+            tamano_empresa = request.POST.get('tamano_empresa')
+            pais = request.POST.get('pais')
+            edad = request.POST.get('edad')
+            estudios = request.POST.get('estudios')
+            genero = request.POST.get('genero')
+            ingles_hablado = request.POST.get('ingles_hablado')
+            ingles_escrito = request.POST.get('ingles_escrito')
+            actividad = request.POST.get('actividad') 
+            contrato = request.POST.get('contrato') 
+            cargo = request.POST.get('cargo')
+            experiencia = request.POST.get('experiencia')
+            rentaLiquida = request.POST.get('rentaLiquida')
+            rentaBono = request.POST.get('rentaBono')
+            duracionTrabajo = request.POST.get('duracionTrabajo')
+            BeneficiosEmpleador = request.POST.get('BeneficiosEmpleador')
+            FactoresCambioEmpleo = request.POST.get('FactoresCambioEmpleo')
+            template= get_template('correo.html')
+            
+            if(nombre==""):
+                messages.error(request, 'Debe ingresar un nombre')
+                context= {'form':form}
+                return render(request,'formulario.html',context)
+            if(email==""):
+                messages.error(request, 'Debe ingresar un email')
+                context= {'form':form}
+                return render(request,'formulario.html',context)
+            if(es_correo_valido(email)==False):
+                messages.error(request, 'Correo Invalido')
+                context= {'form':form}
+                return render(request,'formulario.html',context)
+            if(len(nombre)>60):
+                messages.error(request, 'el nombre ingresado es muy largo ')
+                context= {'form':form}
+                return render(request,'formulario.html',context)
+            if(len(email)>100):
+                messages.error(request, 'el email ingresado es muy largo ')
+                context= {'form':form}
+                return render(request,'formulario.html',context)
+            if(tamano_empresa=='Seleccionar'or pais=='Seleccionar'or edad=='Seleccionar' or estudios=='Seleccionar' or 
+                    genero=='Seleccionar' or ingles_hablado=='Seleccionar' or ingles_escrito=='Seleccionar' or actividad=='Seleccionar' or
+                    contrato=='Seleccionar' or cargo=='Seleccionar' or experiencia=='Seleccionar' or rentaBono=='Seleccionar' or 
+                    duracionTrabajo=='Seleccionar'):
+                        messages.error(request, 'Completa todos los campos antes de enviar')
+                        
+                        context= {'form':form}
+                        return render(request,'formulario.html',context)
+            if Persona.objects.filter(email=request.POST['email']).exists():
+                messages.error(request, 'Ya ha sido enviado un informe al correo ingresado')
+                context= {'form':form}
+                return render(request,'formulario.html',context)
+        except :
+            messages.error(request, 'Error inesperado, intente mas tarde')
             context= {'form':form}
             return render(request,'formulario.html',context)
-        if(email==""):
-            messages.error(request, 'Debe ingresar un email')
-            context= {'form':form}
-            return render(request,'formulario.html',context)
-        if(es_correo_valido(email)==False):
-            messages.error(request, 'Correo Invalido')
-            context= {'form':form}
-            return render(request,'formulario.html',context)
-        if(len(nombre)>60):
-            messages.error(request, 'el nombre ingresado es muy largo ')
-            context= {'form':form}
-            return render(request,'formulario.html',context)
-        if(len(email)>100):
-            messages.error(request, 'el email ingresado es muy largo ')
-            context= {'form':form}
-            return render(request,'formulario.html',context)
-        if(tamano_empresa=='Seleccionar'or pais=='Seleccionar'or edad=='Seleccionar' or estudios=='Seleccionar' or 
-                genero=='Seleccionar' or ingles_hablado=='Seleccionar' or ingles_escrito=='Seleccionar' or actividad=='Seleccionar' or
-                 contrato=='Seleccionar' or cargo=='Seleccionar' or experiencia=='Seleccionar' or rentaBono=='Seleccionar' or 
-                  duracionTrabajo=='Seleccionar'):
-                    messages.error(request, 'Completa todos los campos antes de enviar')
-                    
-                    context= {'form':form}
-                    return render(request,'formulario.html',context)
-        if Persona.objects.filter(email=request.POST['email']).exists():
-            messages.error(request, 'Ya ha sido enviado un informe al correo ingresado')
-            context= {'form':form}
-            return render(request,'formulario.html',context)
+
         if form.is_valid():
             try:
                 context1 = {
