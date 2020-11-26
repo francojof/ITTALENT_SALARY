459e1372dc6086c7bb5ee3125de74153d66de924
francojof <francojof2@gmail.com>
2020-11-26 15:12:45 -0300

    cambios nombre regex

--- formularioPersona/views.py
+++ formularioPersona/views.py
@@ -63,7 +63,11 @@ def index(request):
                 context= {'form':form}
                 return render(request,'formulario.html',context)
             if(es_correo_valido(email)==False):
-                messages.error(request, 'Correo Invalido')
+                messages.error(request, 'Correo invalido')
+                context= {'form':form}
+                return render(request,'formulario.html',context)
+            if(validar_nombre(nombre)==False):
+                messages.error(request, 'Nombre invalido')
                 context= {'form':form}
                 return render(request,'formulario.html',context)
             if(len(nombre)>60):
@@ -139,3 +143,6 @@ def index(request):
 def es_correo_valido(correo):
     expresion_regular = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
     return re.match(expresion_regular, correo) is not None
+def validar_nombre(nombre):
+    nombre_expresion = ([A-Z ])
+    return re.match(nombre_expresion,nombre)
