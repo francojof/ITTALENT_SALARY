9438f5ef270f103c6b079e0f1b0962e6ffb0c485
francojof <francojof2@gmail.com>
2020-11-30 16:11:39 -0300

    test nombre

--- formularioPersona/views.py
+++ formularioPersona/views.py
@@ -69,6 +69,10 @@ def index(request):
                 messages.error(request, 'Correo invalido')
                 context= {'form':form}
                 return render(request,'formulario.html',context)
+            if(validar_nombre(nombre)==False):
+                messages.error(request, 'Nombre invalido')
+                context= {'form':form}
+                return render(request,'formulario.html',context)
             if(len(nombre)>60):
                 messages.error(request, 'El nombre ingresado es muy largo ')
                 context= {'form':form}
@@ -143,5 +147,5 @@ def es_correo_valido(correo):
     expresion_regular = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
     return re.match(expresion_regular, correo) is not None
 def validar_nombre(nombre):
-    nombre_expresion = r"([A-Z ])"
-    return re.match(nombre_expresion,nombre)
+    expresion = r"[A-Z ]"
+    return re.match(expresion, nombre) is not None
